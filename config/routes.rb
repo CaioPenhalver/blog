6afa273762bb70da4a3e17ede842ae3740982657
Caio Penhalver <cpenhalver@gmail.com>
2017-01-04 15:31:48 -0200

    Blog working properly without uploading pictures

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   #get 'login' => 'user_session#new'
   #post 'login' => 'user_session#create'
   #delete 'logout' => 'user_sessions#destroy'
-  root 'posts#index'
+  root 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
